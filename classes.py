edb4b82ffdf2d22e8d43d1237edb1141d87688c4
Florian DELRIEU <florian.delrieu@free.fr>
2024-05-24 12:25:11 +0200

    [0.4] - Lines for detections - draw lines - now blob are circles

--- classes.py
+++ classes.py
@@ -1,7 +1,8 @@
 import pygame
 import random
+import math
 import numpy as np
-from pygame_init import width,height,RED,GREEN,FPS
+from pygame_init import width,height,RED,GREEN,FPS,window,BLACK
 
 class blob:
     def __init__(self,x=None,y=None,is_predator=False):
@@ -17,13 +18,14 @@ class blob:
         self.iteration = 0
 
     def draw(self, window):
-        pygame.draw.rect(window, self.color, (self.x, self.y, self.size, self.size))
+        pygame.draw.circle(window, self.color, (self.x, self.y), self.size)
+        if self.is_predator: self.draw_rays(window)
 
     def move(self,method = "random"):
         if not self.is_predator:    method = "random"
         if self.is_predator:        method = "predator"
         self.iteration += 1
-        self.keep_in_screen()
+        #self.keep_in_screen()
         if method == "random":
             self.x += self.speed * np.cos(self.direction)
             self.y += self.speed * np.sin(self.direction)
@@ -33,8 +35,16 @@ class blob:
         if method == "predator":
             pass
 
+    def draw_rays(self, window, ray_length=100, num_rays=8):
+        angle_step = 360 / num_rays
+        for i in range(num_rays):
+            angle = math.radians(i * angle_step)
+            end_x = self.x + ray_length * math.cos(angle)
+            end_y = self.y + ray_length * math.sin(angle)
+            pygame.draw.line(window, BLACK, (self.x, self.y), (end_x, end_y),width=1)
+
     def keep_in_screen(self):
-        if self.x < 0: self.x = width
-        if self.y < 0: self.x = height
-        if self.x > width: self.x = 0
-        if self.y > height: self.y = 0
+        # todo fix this
+        # Assurer que les blobs restent dans la fenêtre
+        self.x = max(0, min(self.x, width - self.size))
+        self.y = max(0, min(self.y, height() - self.size))
